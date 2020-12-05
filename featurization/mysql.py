6dfd5aff95cc692670935b8ced2b998ca05a0cef
Michael Man <17michaelm@gmail.com>
2020-12-05 06:29:08 -0800

    Changed mysql pass

--- featurization/mysql.py
+++ featurization/mysql.py
@@ -6,7 +6,7 @@ import pymysql.cursors
 # Connect to the database
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='vn5%%opdQ7Yk',
+                             password='Qawsedrftg9!',
                              db='reddit_data',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
